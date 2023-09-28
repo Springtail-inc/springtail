5c352af1c0f15b2da53264779bb4508c2802e389
Craig Soules <craig@craigs-mbp.lan>
2023-09-28 08:33:40 -0700

    Exceptions with traces along with an example

--- include/common/exception.hh
+++ include/common/exception.hh
@@ -1,13 +1,30 @@
 #pragma once
 
+#include <backward.hpp>
+
+// declaration of signal handling variable
+namespace backward {
+    extern backward::SignalHandling sh;
+}
 
 namespace st_common {
     /** 
-     * Base error class for exceptions.  When in debug mode uses cpptrace::exception to capture stack traces.
+     * Base error class for exceptions.  Uses backward for stack tracing.
      */
-#ifdef NDEBUG
-    typedef std::exception Error;
-#else
-    typedef cpptrace::exception Error;
-#endif
+    class Error : public std::exception {
+    private:
+        backward::StackTrace _trace;
+
+    public:
+        /** Captures the stack trace on construction. */
+        Error() {
+            _trace.load_here();
+        }
+
+        /** Prints the backtrace captured by this exception. */
+        void print_trace() {
+            backward::Printer printer;
+            printer.print(_trace);
+        }
+    };
 }
--- src/benchmarks/crash.cc
+++ src/benchmarks/crash.cc
@@ -1,15 +1,26 @@
 #include <iostream>
 
-#include <backward.hpp>
-namespace backward {
-    backward::SignalHandling sh;
-}
+#include <common/exception.hh>
 
 void crash(const char *str) {
     std::cout << *str << std::endl;
 }
 
+void error() {
+    throw st_common::Error();
+}
+
 int main() {
+    // reference the global to force linker to resolve
+    backward::sh.loaded();
+
+    std::cout << "Throw!" << std::endl;
+    try {
+        error();
+    } catch (st_common::Error &e) {
+        e.print_trace();
+    }
+
     std::cout << "Crash!" << std::endl;
     crash(nullptr);
     return 0;
