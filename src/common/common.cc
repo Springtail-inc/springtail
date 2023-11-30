09ccb414d58fb12209674a1b14ffb2179c4fa77d
Garth Goodson <garth@springtail.io>
2023-11-30 12:41:32 -0800

    Initial commit of redis client. (#8)
    * Initial commit of redis client.
    
    * Added a get_to() and get() commands to Json helper class.

--- src/common/common.cc
+++ src/common/common.cc
@@ -1,6 +1,10 @@
+#include <filesystem>
+#include <string>
+
 #include <common/common.hh>
 #include <common/exception.hh>
 #include <common/logging.hh>
+#include <common/properties.hh>
 
 namespace springtail {
     void springtail_init() {
@@ -9,5 +13,12 @@ namespace springtail {
 
         // initialize the logging infrastructure
         init_logging();
+
+        // init system properties
+        if (std::filesystem::exists(std::filesystem::path(Properties::SPRINGTAIL_PROPERTIES_FILE))) {
+            Properties::init(std::string(Properties::SPRINGTAIL_PROPERTIES_FILE));
+        } else {
+            throw Error("Springtail system.json properties file not found");
+        }
     }
 }
