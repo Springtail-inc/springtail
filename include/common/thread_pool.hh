8b8bf59eab6f03ac63a128c7afc78b99083f3b0b
Garth Goodson <garth@springtail.io>
2023-12-02 19:14:39 -0800

    Fixed compile issues. Moved template defn to header.

--- include/common/thread_pool.hh
+++ include/common/thread_pool.hh
@@ -7,6 +7,7 @@
 #include <vector>
 #include <functional>
 #include <condition_variable>
+#include <iostream>
 
 #include <common/exception.hh>
 
@@ -33,13 +34,41 @@ namespace springtail {
          * @brief Push an IO request onto the worker pool queue
          * @param request IO request to enqueue
          */
-        void push(std::shared_ptr<ThreadRequest> request);
+        void push(std::shared_ptr<ThreadRequest> request)
+        {
+            // lock queue lock
+            std::scoped_lock<std::mutex> queue_lock(_mutex);
+
+            // push request onto queue and notify a single thread
+            _queue.push(request);
+            _cv.notify_one();
+        }
 
         /**
          * @brief Pop an IO request off the worker pool queue; or block if empty
          * @return std::shared_ptr<IORequest> ptr to the IORequest
          */        
-        std::shared_ptr<ThreadRequest> pop();
+        std::shared_ptr<ThreadRequest> pop()
+        {
+            // lock queue lock
+            std::unique_lock<std::mutex> queue_lock(_mutex);
+
+            // block until queue is not empty
+            // or until queue is signalled to wake all
+            while (_queue.empty() && !_shutdown) {
+                _cv.wait(queue_lock);
+            }
+
+            // extract first element from queue and return it
+            if (_queue.empty()) {
+                return nullptr;
+            }
+
+            std::shared_ptr<ThreadRequest> val = _queue.front();
+            _queue.pop();
+
+            return val;
+        }
 
         /**
          * @brief Set shutdown flag; wake all blocked workers
@@ -65,7 +94,24 @@ namespace springtail {
          * @param queue  Thread queue
          */
         static void worker_fn(std::shared_ptr<ThreadWorker<ThreadRequest>> worker,
-                              ThreadQueue<ThreadRequest> &queue);
+                              ThreadQueue<ThreadRequest> &queue)
+        {
+            std::cout << "Thread starting: " << std::this_thread::get_id() << std::endl;
+            while (true) {
+                std::shared_ptr<ThreadRequest> request = queue.pop();
+                // only get a nullptr if queue is empty, if so check for shutdown
+                if (request == nullptr) {
+                    if (worker->is_shutdown()) {
+                        return;
+                    }
+                    continue;
+                }
+
+                // process request
+                (*request)();
+            }
+            std::cout << "Thread exiting: " << std::this_thread::get_id() << std::endl;
+        }
 
     public:
 
@@ -118,7 +164,14 @@ namespace springtail {
          * @brief Construct a new Thread Pool object
          * @param max_threads number of threads to create
          */
-        ThreadPool(int max_threads);
+        ThreadPool(int max_threads)
+        {
+            for (int i = 0; i < max_threads; i++) {
+                std::shared_ptr<ThreadWorker<ThreadRequest>> worker = std::make_shared<ThreadWorker<ThreadRequest>>();
+                _workers.push_back(worker);
+                _threads.push_back(std::thread(worker->get_worker_fn(), worker, std::ref(_queue)));
+            }
+        }
 
         /**
          * @brief Destroy the Thread Pool object; shuts down all threads if not already
@@ -128,13 +181,43 @@ namespace springtail {
         /**
          * @brief Shutdown thread pool; pool can not be used after this call
          */
-        void shutdown();
+        void shutdown()
+        {
+            if (_shutdown) {
+                return;
+            }
+
+            // set shutdown flag
+            _shutdown = true;
+
+            // issue shutdown to workers
+            for (auto worker: _workers) {
+                worker->shutdown();
+            }
+
+            // issue shutdown to queue (this will drain the queue)
+            _queue.shutdown();
+
+            // join the threads and remove them from the vector
+            while (!_threads.empty()) {
+                std::thread &t = _threads.back();
+                std::cout << "Thread joining: " << t.get_id() << std::endl;
+                t.join();
+                _threads.pop_back();
+            }
+        }
 
         /**
          * @brief Queue the thread request
          * @param request request to queue
          */
-        void queue(std::shared_ptr<ThreadRequest> request);
+        void queue(std::shared_ptr<ThreadRequest> request)
+        {
+            if (_shutdown) {
+                throw Error("Pool has shutdown\n");
+            }
+            _queue.push(request);
+        }
 
     private:
         /** thread queue */
