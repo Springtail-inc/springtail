aef0d6373f4b3c295232abe8778ca0bafa0ad132
Garth Goodson <garth@springtail.io>
2024-02-14 15:17:59 -0800

    Added queue tests.

--- src/common/common.cc
+++ src/common/common.cc
@@ -7,6 +7,29 @@
 #include <common/properties.hh>
 
 namespace springtail {
+
+    void init_system_properties()
+    {
+        if (std::filesystem::exists(std::filesystem::path(Properties::SPRINGTAIL_PROPERTIES_FILE))) {
+            Properties::init(std::string(Properties::SPRINGTAIL_PROPERTIES_FILE));
+            return;
+        }
+
+        // check current dir
+        if (std::filesystem::exists(std::filesystem::path("./system.json"))) {
+            Properties::init("./system.json");
+            return;
+        }
+
+        const char *env_p = std::getenv("SPRINGTAIL_PROPERTIES_FILE");
+        if (env_p && std::filesystem::exists(std::filesystem::path(env_p))) {
+            Properties::init(env_p);
+            return;
+        }
+
+        throw Error("Springtail system.json properties file not found");
+    }
+
     void springtail_init() {
         // initialize the backtrace infrastructure
         backward::sh.loaded();
@@ -15,10 +38,6 @@ namespace springtail {
         init_logging();
 
         // init system properties
-        if (std::filesystem::exists(std::filesystem::path(Properties::SPRINGTAIL_PROPERTIES_FILE))) {
-            Properties::init(std::string(Properties::SPRINGTAIL_PROPERTIES_FILE));
-        } else {
-            throw Error("Springtail system.json properties file not found");
-        }
+        init_system_properties();
     }
 }
---
+++ src/common/test/test_queue.cc
@@ -0,0 +1,111 @@
+#include <memory>
+#include <gtest/gtest.h>
+
+#include <common/concurrent_queue.hh>
+
+using namespace springtail;
+
+struct QueueEntry {
+    int a;
+    QueueEntry(int a) : a(a) {}
+};
+
+TEST(QueueTest, SimpleTest) {
+    ConcurrentQueue<QueueEntry> queue{};
+    queue.push(std::make_shared<QueueEntry>(1));
+    queue.push(std::make_shared<QueueEntry>(2));
+    queue.push(std::make_shared<QueueEntry>(3));
+    queue.push(std::make_shared<QueueEntry>(4));
+    queue.push(std::make_shared<QueueEntry>(5));
+
+    auto entry = queue.pop();
+    EXPECT_EQ(entry->a, 1);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 2);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 3);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 4);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 5);
+}
+
+TEST(QueueTest, ArrayTest) {
+    ConcurrentQueue<QueueEntry> queue{};
+    std::vector<std::shared_ptr<QueueEntry>> entries;
+    entries.push_back(std::make_shared<QueueEntry>(1));
+    entries.push_back(std::make_shared<QueueEntry>(2));
+    entries.push_back(std::make_shared<QueueEntry>(3));
+    entries.push_back(std::make_shared<QueueEntry>(4));
+    entries.push_back(std::make_shared<QueueEntry>(5));
+    queue.push(entries);
+
+    auto entry = queue.pop();
+    EXPECT_EQ(entry->a, 1);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 2);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 3);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 4);
+    entry = queue.pop();
+    EXPECT_EQ(entry->a, 5);
+}
+
+TEST(QueueTest, ThreadedTestArrayBound) {
+    ConcurrentQueue<QueueEntry> queue{3};
+    std::vector<std::shared_ptr<QueueEntry>> entries;
+    entries.push_back(std::make_shared<QueueEntry>(1));
+    entries.push_back(std::make_shared<QueueEntry>(2));
+    entries.push_back(std::make_shared<QueueEntry>(3));
+    entries.push_back(std::make_shared<QueueEntry>(4));
+    entries.push_back(std::make_shared<QueueEntry>(5));
+
+    std::thread t1([&queue, &entries](){
+        queue.push(entries);
+    });
+
+    std::thread t2([&queue](){
+        auto entry = queue.pop();
+        EXPECT_EQ(entry->a, 1);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 2);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 3);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 4);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 5);
+    });
+
+    t1.join();
+    t2.join();
+}
+
+TEST(QueueTest, ThreadedTestBound) {
+    ConcurrentQueue<QueueEntry> queue{3};
+
+    std::thread t1([&queue](){
+        queue.push(std::make_shared<QueueEntry>(1));
+        queue.push(std::make_shared<QueueEntry>(2));
+        queue.push(std::make_shared<QueueEntry>(3));
+        queue.push(std::make_shared<QueueEntry>(4));
+        queue.push(std::make_shared<QueueEntry>(5));
+    });
+
+    std::thread t2([&queue](){
+        auto entry = queue.pop();
+        EXPECT_EQ(entry->a, 1);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 2);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 3);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 4);
+        entry = queue.pop();
+        EXPECT_EQ(entry->a, 5);
+    });
+
+    t1.join();
+    t2.join();
+}
