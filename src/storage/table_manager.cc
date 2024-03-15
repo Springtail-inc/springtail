2b674b895576b8ea96d3158044cd2a773a88eef4
Craig Soules <craig@craigssgtailmac.lan>
2024-03-15 14:21:41 -0700

    Checkpoint #15

--- src/storage/table_manager.cc
+++ src/storage/table_manager.cc
@@ -28,22 +28,18 @@ namespace springtail {
         key_fields->at(2) = std::make_shared<ConstTypeField<uint64_t>>(lsn);
         auto search_key = std::make_shared<FieldTuple>(key_fields);
 
-        auto pos_i = tables_t->primary_lower_bound(search_key);
-
-        auto schema = tables_t->get_schema(xid, lsn);
-        auto fields = schema->get_fields("table_id", "xid", "lsn");
-
-        // if the lower_bound() found a greater entry, we need to go back one to find the row that
-        // covers this XID/LSN
-        if (search_key->less_than(FieldTuple(fields, *pos_i))) {
-            --pos_i;
-        }
-
+        // XXX specialized search that finds the entry directly before the upper_bound(search_key)
+        auto pos_i = tables_t->primary->inverted_upper_bound(search_key);
         if (pos_i == tables_t->primary->end()) {
             return nullptr;
         }
 
+        auto schema = tables_t->get_schema(xid, lsn);
+        auto fields = schema->get_fields("table_id", "xid", "lsn");
+
+        // verify that the returned entry references the requested table
         if (fields->at(0)->get_uint64(*pos_i) != table_id) {
+            // if not, then the table does not exist at the provided XID/LSN
             return nullptr;
         }
 
@@ -94,12 +90,46 @@ namespace springtail {
             name_f->get_text(row) != msg.table) {
             // 1) if the table name is changed, add two rows to the "tables" -- one with the new name, and one to remove the old name
             //    XXX need to find the old name
+            auto key_fields = std::make_shared<FieldArray>(3);
+            key_fields->at(0) = std::make_shared<ConstTypeField<uint64_t>>(msg.oid);
+            key_fields->at(1) = std::make_shared<ConstTypeField<uint64_t>>(xid);
+            key_fields->at(2) = std::make_shared<ConstTypeField<uint64_t>>(lsn);
+            auto search_key = std::make_shared<FieldTuple>(key_fields);
+
+            auto secondary = tables_t->secondary["by_table_id"];
+            pos_i = secondary->inverted_upper_bound(search_key);
+            if (pos_i == secondary->end() || fields->at(0)->get_uint64(*pos_i) != msg.oid) {
+                // XXX some kind of error finding the old name
+            }
+
+            auto extent_id = fields->at(EXTENT_ID)->get_uint64(*pos_i);
+            auto row_id = fields->at(ROW_ID)->get_uint64(*pos_i);
+            auto extent = _read_extent(extent_id);
+            auto row = extent->at(row_id);
+            std::string old_name = fields->at(NAME)->get_text(row);
+            std::string old_namespace = fields->at(NAMESPACE)->get_text(row);
 
             // XXX insert the new name with this oid
-            tables_t->insert();
+            auto new_fields = std::make_shared<FieldArray>(7);
+            new_fields->at(TABLE_ID) = std::make_shared<ConstTypeField<uint64_t>>(msg.oid);
+            new_fields->at(XID) = std::make_shared<ConstTypeField<uint64_t>>(xid);
+            new_fields->at(LSN) = std::make_shared<ConstTypeField<uint64_t>>(lsn);
+            new_fields->at(NAME) = std::make_shared<ConstTypeField<std::string>>(msg.table);
+            new_fields->at(NAMESPACE) = std::make_shared<ConstTypeField<std::string>>(msg.schema);
+            new_fields->at(EXISTS) = std::make_shared<ConstTypeField<bool>>(true);
+
+            tables_t->insert(FieldTuple(new_fields, nullptr));
 
             // XXX insert the old name with exists = false
-            tables_t->insert();
+            auto old_fields = std::make_shared<FieldArray>(7);
+            old_fields->at(TABLE_ID) = std::make_shared<ConstTypeField<uint64_t>>(msg.oid);
+            old_fields->at(XID) = std::make_shared<ConstTypeField<uint64_t>>(xid);
+            old_fields->at(LSN) = std::make_shared<ConstTypeField<uint64_t>>(lsn);
+            old_fields->at(NAME) = std::make_shared<ConstTypeField<std::string>>(old_name);
+            old_fields->at(NAMESPACE) = std::make_shared<ConstTypeField<std::string>>(old_namespace);
+            old_fields->at(EXISTS) = std::make_shared<ConstTypeField<bool>>(false);
+
+            tables_t->insert(FieldTuple(old_fields, nullptr));
         } else {
             // 2) determine the set of changes between the provided schema and the prior schema
             auto old_schema = _schema_manager->get_schema(msg.oid, xid, lsn);
@@ -122,7 +152,22 @@ namespace springtail {
         // 1) update the "tables" with a drop entry
         //    note: the GC-3 should evict these entries once the data has been brought forward and
         //          we can assume the table_id won't be re-used until that operation is complete
-        tuple = msg.oid, msg.schema, msg.table, xid, lsn, false;
-        tables_t->insert(tuple);
+        auto fields = std::make_shared<FieldArray>(6);
+        fields->at(TABLE_ID) = std::make_shared<ConstTypeField<int64_t>>(msg.oid);
+        fields->at(NAMESPACE) = std::make_shared<ConstTypeField<std::string>>(msg.schema);
+        fields->at(NAME) = std::make_shared<ConstTypeField<std::string>>(msg.table);
+        fields->at(XID) = std::make_shared<ConstTypeField<int64_t>>(xid);
+        fields->at(LSN) = std::make_shared<ConstTypeField<int64_t>>(lsn);
+        fields->at(EXISTS) = std::make_shared<ConstTypeField<bool>>(false);
+
+        tables_t->insert(FieldTuple(fields, nullptr));
+
+        // update the "table_roots" with a null entry
+        auto fields = std::make_shared<FieldArray>(3);
+        fields->at(TABLE_ID) = std::make_shared<ConstTypeField<int64_t>>(msg.oid);
+        fields->at(XID) = std::make_shared<ConstTypeField<int64_t>>(xid);
+        fields->at(EXTENT_ID) = std::make_shared<ConstNullField>(SchemaType::UINT64);
+
+        table_roots_t->insert(FieldTuple(fields, nullptr));
     }
 }
