73ca8fd1e87143f3ddc6d8984222ed338160cd76
Garth Goodson <garth@springtail.io>
2024-04-16 09:28:49 -0700

    #40 added fix to build vcpkg with gcc-13 on mac

--- vcpkg.sh
+++ vcpkg.sh
@@ -4,8 +4,18 @@ DIR=external/vcpkg
 
 if [ ! -d ${DIR} ]
 then
-   git clone https://github.com/Microsoft/vcpkg.git "${DIR}"
-   cd "$DIR"
+    if [[ $(uname -s) == "Darwin" ]]; then
+        echo "Running on macOS"
+        export CC=`which gcc-13`
+        export CXX=`which g++-13`
+    else
+        echo "Not running on macOS"
+        export CC=`which gcc`
+        export CXX=`which g++`
+    fi
+
+    git clone https://github.com/Microsoft/vcpkg.git "${DIR}"
+    cd "$DIR"
    ./bootstrap-vcpkg.sh
    ./vcpkg integrate install
 else
