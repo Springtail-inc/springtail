a6cbe025d5e293391d8b23406ba9885c64ce7dc0
Garth Goodson <garth@springtail.io>
2024-05-09 15:11:31 -0700

    #40 updated to gcc-14

--- cmake/FindCompiler.cmake
+++ cmake/FindCompiler.cmake
@@ -18,7 +18,7 @@ function(check_gcc_function RESULT_VAR COMPILER)
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    # make sure the output of --version does not contain "clang" and is >= 13.0
+    # make sure the output of --version does not contain "clang" and is >= 13.2
     string(REGEX MATCH "clang" HAS_CLANG ${GCC_FULL_VERSION})
 
     # use -dumpversion to get actual version number
@@ -28,7 +28,7 @@ function(check_gcc_function RESULT_VAR COMPILER)
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    if(NOT GCC_MAJOR_VERSION OR GCC_MAJOR_VERSION LESS 13)
+    if(NOT GCC_MAJOR_VERSION OR GCC_MAJOR_VERSION LESS 14)
         set(${RESULT_VAR} FALSE PARENT_SCOPE)
     endif()
 
@@ -49,21 +49,21 @@ set(SEARCH_PATHS
 
 # Find the GCC compiler
 find_program(GCC_C_COMPILER
-    NAMES gcc gcc-13 gcc-14
+    NAMES gcc gcc-14
     HINTS ${SEARCH_PATHS} ENV PATH
     VALIDATOR check_gcc_function
 )
 
 # Find the G++ compiler
 find_program(GCC_CXX_COMPILER
-    NAMES g++ g++-13 g++-14
+    NAMES g++ g++-14
     HINTS ${SEARCH_PATHS} ENV PATH
     VALIDATOR check_gcc_function
 )
 
 # Check if the GCC compiler was found
 if (NOT GCC_C_COMPILER OR NOT GCC_CXX_COMPILER)
-    message(FATAL_ERROR "GCC compiler >= 13.0 not found!")
+    message(FATAL_ERROR "GCC compiler >= 14.0 not found!")
 else()
     message(STATUS "GCC compiler found: ${GCC_C_COMPILER}")
     message(STATUS "GCC++ compiler found: ${GCC_CXX_COMPILER}")
