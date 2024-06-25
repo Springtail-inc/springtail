c951df5aed4c46526aea866220a80a0a639198c7
Craig Soules <craig@craigssgtailmac.lan>
2024-06-24 21:06:23 -0700

    Added scripts for setting up an end-to-end ingest environment

---
+++ scripts/local/cleanup.sql
@@ -0,0 +1,21 @@
+-- drop the logical replica database
+DROP DATABASE IF EXISTS springtail_replica;
+
+-- remove the FDW
+DROP SERVER IF EXISTS springtail_fdw_server;
+DROP EXTENSION IF EXISTS springtail_fdw;
+
+-- clear the replication triggers
+DROP EVENT TRIGGER IF EXISTS springtail_event_trigger_for_drops;
+DROP EVENT TRIGGER IF EXISTS springtail_event_trigger_for_ddl;
+
+DROP FUNCTION IF EXISTS springtail_add_replica_identity_full;
+DROP FUNCTION IF EXISTS springtail_event_trigger_for_drops;
+DROP FUNCTION IF EXISTS springtail_event_trigger_for_ddl;
+
+-- remove the replication setup
+SELECT pg_drop_replication_slot('springtail_slot');
+DROP PUBLICATION IF EXISTS springtail_pub;
+
+-- remove the test data
+DROP TABLE test_data;
