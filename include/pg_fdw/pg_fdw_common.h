b5bafb15a965b32b58c9e45c96be2eaf0e6f4c97
Garth Goodson <garth@springtail.io>
2024-06-26 12:04:27 -0700

    #22 converted tabs to spaces.

--- include/pg_fdw/pg_fdw_common.h
+++ include/pg_fdw/pg_fdw_common.h
@@ -32,46 +32,46 @@ typedef struct SpringtailPlanState {
 /** Sort group */
 typedef struct DeparsedSortGroup
 {
-    Name 			attname;
-    int				attnum;
-    bool			reversed;
-    bool			nulls_first;
-    Name			collate;
-    PathKey	       *key;
+    Name           attname;
+    int            attnum;
+    bool           reversed;
+    bool           nulls_first;
+    Name           collate;
+    PathKey       *key;
 } DeparsedSortGroup;
 
 /** Base qual for predicates */
 typedef struct BaseQual
 {
-    AttrNumber	varattno;
-    NodeTag		right_type;
-    Oid			typeoid;
-    char	   *opname;
-    bool		isArray;
-    bool		useOr;
-}	BaseQual;
+    AttrNumber     varattno;
+    NodeTag        right_type;
+    Oid            typeoid;
+    char          *opname;
+    bool           isArray;
+    bool           useOr;
+} BaseQual;
 
 /** Constant predicate */
 typedef struct ConstQual
 {
-    BaseQual    base;
-    Datum		value;
-    bool		isnull;
-}	ConstQual;
+    BaseQual       base;
+    Datum          value;
+    bool           isnull;
+} ConstQual;
 
 /** Variable predicate */
 typedef struct VarQual
 {
-    BaseQual    base;
-    AttrNumber	rightvarattno;
-}	VarQual;
+    BaseQual       base;
+    AttrNumber     rightvarattno;
+} VarQual;
 
 /** Parameterized predicate */
 typedef struct ParamQual
 {
-    BaseQual    base;
-    Expr	   *expr;
-}	ParamQual;
+    BaseQual       base;
+    Expr          *expr;
+} ParamQual;
 
 
 #ifdef __cplusplus
