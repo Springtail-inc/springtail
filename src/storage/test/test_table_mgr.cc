07945f3db2749b36547f8308457c5569a0dbfc9a
Craig Soules <craig@craigssgtailmac.lan>
2024-08-12 22:20:00 -0700

    Compiling again after merge down from main #88

--- src/storage/test/test_table_mgr.cc
+++ src/storage/test/test_table_mgr.cc
@@ -20,14 +20,7 @@ namespace {
     class TableMgr_Test : public testing::Test {
         void SetUp() override {
             springtail_init();
-
-            auto json = Properties::get(Properties::STORAGE_CONFIG);
-            Json::get_to<std::filesystem::path>(json, "table_dir", _table_dir);
-            _table_dir = Properties::make_absolute_path(_table_dir);
-
             _services.init(true);
-
-            std::filesystem::remove_all(_table_dir);
         }
 
         void TearDown() override {
