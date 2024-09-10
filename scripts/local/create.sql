bcb293ade47f77c989bf3aa605ceff8a18ffadbc
Craig Soules <craig@springtail.io>
2024-09-09 22:03:10 -0700

    Support for tables without primary keys -- tested end-to-end #117

--- scripts/local/create.sql
+++ scripts/local/create.sql
@@ -11,5 +11,6 @@ CREATE TABLE test_data4 (a INT PRIMARY KEY, b TEXT, c TIMESTAMP, d JSON);
 INSERT INTO test_data4 (a, b, c, d) VALUES (1, 'a', now(), '{"bar": "baz", "balance": 7.77, "active": false}'::json);
 
 CREATE TABLE test_data5 (a INT, b TEXT, c TIMESTAMP);
+ALTER TABLE test_data5 REPLICA IDENTITY FULL;
 INSERT INTO test_data5 (a, b, c) VALUES (1, 'a', now());
 INSERT INTO test_data5 (a, b, c) VALUES (2, 'b', now());
--- scripts/local/test.sql
+++ scripts/local/test.sql
@@ -1,4 +1,6 @@
 \set AUTOCOMMIT off
+
+-- Test INSERT
 BEGIN;
 INSERT INTO test_data4 (a, b) VALUES (2, 'two');
 INSERT INTO test_data4 (a, b, c) VALUES (3, 'three', now());
@@ -6,29 +8,31 @@ INSERT INTO test_data4 (a, b, c) VALUES (4, 'four', now());
 INSERT INTO test_data4 (a, b) VALUES (5, 'five');
 INSERT INTO test_data4 (a, b, d) VALUES (6, 'six', '{"bar": "baz", "balance": 7.77, "active": false}'::json);
 COMMIT;
+
+-- Test UPDATE
 BEGIN;
 UPDATE test_data4 SET b = 'one' WHERE a = 1;
 COMMIT;
 
+-- Test DELETE
 BEGIN;
 DELETE FROM test_data4 WHERE a = 5;
 COMMIT;
 
+-- Test UPDATE to a record that was INSERT in the same transaction
 BEGIN;
 INSERT INTO test_data4 (a, b, c) VALUES (5, 'fiv', now());
 UPDATE test_data4 SET b = 'fivefive' WHERE a = 5;
 COMMIT;
 
+-- Test array types
 BEGIN;
 INSERT INTO test_data2 (a, b) VALUES ('{1, 3}', 'b');
 INSERT INTO test_data2 (a, b) VALUES ('{1, 3, 5}', 'c');
 INSERT INTO test_data2 (a, b) VALUES ('{1, 8, 5}', 'd');
 COMMIT;
 
--- BEGIN;
--- ALTER TABLE test_data2 ADD COLUMN e INTEGER DEFAULT VALUE 10;
--- COMMIT;
-
+-- Test removing and adding a column within a single transaction
 BEGIN;
 INSERT INTO test_data3 (b, d) VALUES ('b', 14);
 ALTER TABLE test_data3 DROP COLUMN d;
@@ -36,4 +40,18 @@ INSERT INTO test_data3 (b, c) VALUES ('c', now());
 ALTER TABLE test_data3 ADD COLUMN e INTEGER;
 INSERT INTO test_data3 (b, e) VALUES ('d', 15);
 UPDATE test_data3 SET e = 14 WHERE b = 'b';
+COMMIT;
+
+-- Test adding and updating records to a table with no primary key
+BEGIN;
+INSERT INTO test_data5 (a, b, c) VALUES (4, 'd', now());
+INSERT INTO test_data5 (a, b, c) VALUES (3, 'c', now());
+UPDATE test_data5 SET b = 'A' WHERE a = 1;
+COMMIT;
+
+-- Test adding duplicate rows to a table with no primary key and then updating them
+BEGIN;
+INSERT INTO test_data5 (a, b, c) VALUES (5, 'e', now());
+INSERT INTO test_data5 (a, b, c) VALUES (5, 'e', now());
+UPDATE test_data5 SET b = 'E' WHERE a = 5;
 COMMIT;
