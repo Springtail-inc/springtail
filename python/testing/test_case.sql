d519f6b1b64789475b412f17eca885e36085ca0b
Luis SprinTail <luisST@Luiss-MacBook-Pro.local>
2024-10-10 16:53:17 -0600

    WIP - Testing Framework

--- python/testing/test_case.sql
+++ python/testing/test_case.sql
@@ -1,18 +1,14 @@
-## setup
 # PHASE 1 -- table creation
 CREATE TABLE test1 (colA text PRIMARY KEY, colB bigint);
 
-## test
 # PHASE 2 -- data modification
 INSERT INTO test1 (colA, colB) VALUES ('a', 1), ('b', 2), ('c', 3);
 INSERT INTO test1 (colA, colB) VALUES ('d', 4);
 UPDATE test1 SET colB = colB + 1 WHERE colA = 'a';
 
-## verify
 # PHASE 3 -- data verification
 SELECT * FROM test1 ORDER BY colA;
 SELECT colA FROM test1 ORDER BY colB;
 
-## cleanup
 # PHASE 4 -- data cleanup
 DROP TABLE test1;
---
+++ python/testing/test_cases/test_state.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS test_state (
+    test_name TEXT PRIMARY KEY,
+    status TEXT
+);
