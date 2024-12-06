6366c74f62f5337ed51fa0353d8f0e4681485550
Craig Soules <craig@springtail.io>
2024-12-05 17:33:51 -0800

    Checkpoint -- refs SPR-400

--- include/pg_log_mgr/pg_log_reader.hh
+++ include/pg_log_mgr/pg_log_reader.hh
@@ -12,9 +12,9 @@
 
 namespace springtail::pg_log_mgr {
     /**
-     * @brief Log reader class.  Reads logs written by PgLogWriter.  Does minimal parsing
-     * to extract begin and commit messages.  Queues those begin/commit messages to a
-     * shared queue for logging and to be sent to the GC.
+     * @brief Log reader class.  Reads logs written by PgLogWriter.  Collects the various table
+     * mutations into batches which are passed to the WriteCache to make them available to the
+     * Committer and FDWs.
      */
     class PgLogReader {
     public:
@@ -48,11 +48,87 @@ namespace springtail::pg_log_mgr {
         }
 
     private:
+        class Batch {
+            // 4 MB
+            constexpr uint32_t MAX_BATCH_SIZE = 4 * 1024 * 1024;
+
+        public:
+            /**
+             * Send all extents to the WriteCache, apply all schema changes, notify the
+             * Committer.
+             */
+            void commit();
+
+            /**
+             * Abort the entire transaction.  Drop all related batches from the WriteCache and from
+             * memory.
+             */
+            void abort();
+
+            /**
+             * Starts a subtransaction by sending all changes thus-far to the WriteCache.  Keeps a
+             * pointer to the schema changes so that they can be dropped in the case of a subtxn
+             * abort.
+             */
+            void start_subtxn();
+
+            /**
+             * Aborts a subtxn by removing those entries from the WriteCache, dropping any in-memory
+             * extents, and rolling back the set of schema changes.
+             */
+            void abort_subtxn();
+
+            /**
+             * Adds a mutation to a given table's batch.
+             */
+            void add_mutation(int32_t tid);
+
+            /**
+             * Records a truncation of a given table into the batch.
+             */
+            void truncate(int32_t tid);
+
+        private:
+            using ChangeList = std::vector<std::pair<PgMsgPtr, uint64_t>>;
+
+            /** Tracks details about a single table within a txn or subtxn. */
+            struct TableEntry {
+                ExtentPtr extent; ///< The batch of changes to the table thus-far.
+                uint64_t start_lsn; ///< The LSN of the first change in the batch.
+                ExtentSchemaPtr schema; ///< The schema of the data stored in the batch.
+                FieldPtr type_f; ///< The field accessor for the mutation's type.
+                FieldPtr lsn_f; ///< The field accessor for the mutation's LSN.
+            };
+            using TableMap = std::map<int32_t, TableEntry>;
+
+            /** Tracks details about a txn or subtxn. */
+            struct TxnEntry {
+                int32_t pg_xid; ///< The pgxid of the subtxn
+                TableMap table_map; ///< Map from table ID to TableEntry
+                ChangeList changes; ///< List of (schema change + LSN) ordered by LSN
+            };
+            using TxnEntryPtr = std::shared_ptr<TxnEntryPtr>;
+
+            std::map<int32_t, TxnEntryPtr> _txns; ///< Map of pgxid to txn details.
+
+            uint64_t _db; ///< The associated database ID
+            int32_t _pg_xid; ///< The top-most pgxid for the transaction
+            int32_t _cur_pg_xid; ///< The current pgxid being processed
+
+            uint64_t _lsn = 0; ///< The LSN counter
+        };
+        using BatchPtr = std::shared_ptr<Batch>;
+
         std::filesystem::path _current_path; ///< current log file path
         PgMsgStreamReader _reader;           ///< msg stream reader for log file
         PgTransactionQueuePtr _queue;        ///< shared queue for xactions
         PgTransactionPtr _current_xact;      ///< current transaction
         std::map<uint32_t, PgTransactionPtr> _xact_map; ///< in progress xact map
+
+        /** Tracks mutation batches using a map of pgxid -> Extent.  The pgxid is always the
+            top-most pgxid and never a subtxn, which are handled within the batch. */
+        std::map<int32_t, BatchPtr> _batch_map;
+        BatchPtr _current_batch; ///< The batch matching the current pg xid
 
         /** Process begin message */
         void _process_begin(const PgMsgBegin &begin_msg);
@@ -68,6 +144,12 @@ namespace springtail::pg_log_mgr {
 
         /** Process stream abort message */
         void _process_stream_abort(const PgMsgStreamAbort &abort_msg);
+
+        /** Process mutations */
+        void _process_mutation(const PgMsgPtr &msg);
+
+        /** Process truncation */
+        void _process_truncate(const PgMsgTruncate &msg);
 
         /** Process ddl change message; add oid to xact oid set */
         void _process_ddl(uint32_t oid, int32_t xid, bool is_streaming);
--- src/pg_log_mgr/pg_log_reader.cc
+++ src/pg_log_mgr/pg_log_reader.cc
@@ -13,6 +13,226 @@
 namespace springtail::pg_log_mgr {
 
     void
+    PgLogReader::Batch::commit()
+    {
+        // go through each subtxn and push it's outstanding batches to the WriteCache
+        for (const auto &txn_entry : _txns) {
+            for (const auto &entry : txn_entry.second.table_map) {
+                WriteCacheFuncImpl::add_extent(_db, entry.first, txn_entry.first,
+                                               entry.second.start_lsn, entry.second.extent);
+            }
+        }
+
+        // XXX need some way to collect *all* of the valid subtxns together
+
+        // XXX assign an XID to the committed transaction and update the mappings in the write cache
+        uint64_t xid = get_xid();
+        WriteCacheFuncImpl::commit(_db, _pg_xids, xid);
+
+        // apply any schema changes to the SysTblMgr
+        auto client = sys_tbl_mgr::Client::get_instance();
+        for (auto &&change : _schema_changes) {
+            nlohmann::json ddl_stmt;
+
+            switch(change->msg_type) {
+            case PgMsgEnum::CREATE_TABLE:
+                {
+                    auto &table_msg = std::get<PgMsgTable>(change->msg);
+                    ddl_stmt = client->create_table(_db, xid, table_msg);
+                    break;
+                }
+            case PgMsgEnum::ALTER_TABLE:
+                {
+                    auto &table_msg = std::get<PgMsgTable>(change->msg);
+                    ddl_stmt = client->alter_table(_db, xid, table_msg);
+
+                    // check for re-sync
+                    nlohmann::json action = nlohmann::json::parse(ddl_stmt).at("action");
+                    if (action.get<std::string>() == "resync") {
+                        // XXX if a re-sync is required for a table, drop the changes for that table
+                        //     from the write cache and initiate a re-sync
+                        throw Error();
+                    }
+                    break;
+                }
+            case PgMsgEnum::DROP_TABLE:
+                {
+                    auto &drop_msg = std::get<PgMsgDropTable>(change->msg);
+                    ddl_stmt = client->drop_table(_db, xid, drop_msg);
+                    break;
+                }
+            case PgMsgEnum::CREATE_INDEX:
+                {
+                    auto &index_msg = std::get<PgMsgIndex>(change->msg);
+                    ddl_stmt = client->create_index(_db, xid, index_msg);
+                    break;
+                }
+            case PgMsgEnum::DROP_INDEX:
+                {
+                    auto &index_msg = std::get<PgMsgDropIndex>(change->msg);
+                    ddl_stmt = client->drop_index(_db, xid, index_msg);
+                    break;
+                }
+            }
+
+            _redis_ddl.add_ddl(_db, xid, ddl_stmt);
+        }
+
+        // XXX message the Committer
+    }
+
+    void
+    PgLogReader::Batch::abort()
+    {
+        // drop any batches for all active txns
+        for (auto &&entry : _txns) {
+            auto txn = entry.second;
+            WriteCacheFuncImpl::abort(_db, txn->pg_xid);
+        }
+    }
+
+    void
+    PgLogReader::Batch::abort_subtxn(int32_t pg_xid)
+    {
+        // find the txn to abort it
+        auto itr = _txns.find(pg_xid);
+        assert(itr != _txns.end());
+
+        // abort any batches associated with this txn sent to the WriteCache
+        WriteCacheFuncImpl::abort(_db, itr->second->pg_xid);
+
+        // note: the schema changes will be lost when this txn is removed from the active set
+        _txns.erase(itr);
+
+        // note: we don't update the current pgxid since then we will have a guaranteed mis-match on
+        //       the next operation and we can handle it accordingly
+    }
+
+    void
+    PgLogReader::Batch::add_mutation(int32_t tid, int32_t pg_xid)
+    {
+        auto txn = _get_txn(pg_xid);
+
+        // get the Extent containing mutations
+        auto &entry = txn->table_map[tid];
+        if (entry.extent == nullptr) {
+            entry.extent = std::make_shared<Extent>(); // XXXXXX
+            entry.start_lsn = _lsn;
+        }
+
+        // add the mutation to the batch
+        auto &&row = entry.extent->append();
+        MutableTuple(entry.fields, row).assign(FieldTuple(entry.pg_fields, &msg.new_tuple));
+        _type_f->set_int8(row, Op::INSERT);
+        _lsn_f->set_uint64(row, _lsn++);
+
+        // if the batch has grown to the maximum size, pass it to the WriteCache
+        if (entry.extent->byte_count() > MAX_BATCH_SIZE) {
+            WriteCacheFuncImpl::add_extent(_db, tid, pg_xid, entry.start_lsn, entry.extent);
+            entry.extent = nullptr;
+        }
+    }
+
+    void
+    PgLogReader::Batch::truncate(const PgMsgTruncate &msg)
+    {
+        // get the current txn
+        auto txn = _get_txn(msg.xid);
+
+        // note: there may be multiple truncates due to CASCADE
+        for (auto tid : msg.rel_ids) {
+            // if we sent any batches to the WriteCache, evict them
+            WriteCacheFuncImpl::drop_table(_db, tid, _cur_pg_xid);
+
+            // XXX if this is a subtxn, then once it's committed, we could also drop any earlier
+            //     mutations to the table in it's parent pg xid
+
+            // create a new extent that begins with a truncate operation
+            auto &entry = txn->table_map[tid];
+            entry.extent = std::make_shared<Extent>();
+            entry.start_lsn = _lsn;
+
+            // add a TRUNCATE row
+            auto &&row = entry.extent->append();
+            _type_f->set_int8(row, Op::TRUNCATE);
+            _lsn_f->set_uint64(row, _lsn++);
+        }
+    }
+
+    void
+    PgLogReader::Batch::schema_change(int32_t tid, int32_t pg_xid, PgMsgPtr msg)
+    {
+        // get the table entry
+        auto txn = _get_txn(pg_xid);
+        auto &entry = txn->table_map[tid];
+
+        // push any data mutations into the write cache since the schema is going to change
+        if (entry.extent) {
+            WriteCacheFuncImpl::add_extent(_db, tid, pg_xid,
+                                           entry.start_lsn, entry.extent);
+            entry.extent = nullptr;
+        }
+
+        // for table creation / mutation create a new schema
+        if (msg->msg_type == PgMsgEnum::CREATE_TABLE || msg->msg_type == PgMsgEnum::ALTER_TABLE) {
+            entry.schema = std::make_shared<ExtentSchema>(schema_change);
+        } else if (msg->msg_type == PgMsgEnum::DROP_TABLE) {
+            // XXX should we do a truncate here?  it could improve performance if this follows a set
+            //     of mutations, but it's not clear we actually need it since the mutations would be
+            //     dropped either way
+        }
+
+        // save the change to process once we see a commit
+        entry.changes.emplace_back({ msg, _lsn++ });
+    }
+
+    TxnEntryPtr
+    PgLogReader::Batch::_get_txn(int32_t pg_xid)
+    {
+        // check if a new subtxn started
+        if (pg_xid == _cur_pg_xid) {
+            return _cur_txn;
+        }
+
+        // see if we already know about this pg_xid
+        auto itr = _txns.find(pg_xid);
+        if (itr == _txns.end()) {
+            return _start_subtxn(pg_xid);
+        }
+
+        return itr->second;
+    }
+
+    TxnEntryPtr
+    PgLogReader::Batch::_start_subtxn(int32_t pg_xid)
+    {
+        // construct a new txn entry for this subtxn
+        auto sub_txn = std::make_shared<TxnEntry>(pg_xid, _schema_changes.end());
+
+        // send the current txn's extents to the write cache and pull the schema forward to the subtxn
+        auto cur_txn = _txns.back();
+        for (auto &entry : cur_txn->table_map) {
+            auto &table = entry.second;
+
+            // send the current extent to the WriteCache
+            WriteCacheFuncImpl::add_extent(_db, entry.first, cur_txn->pg_xid,
+                                           table.start_lsn, table.extent);
+            table.extent = nullptr;
+
+            // use the schema information from the current txn in the subtxn
+            sub_txn->table_map.try_emplace(entry.first, { table.schema });
+        }
+
+        // record the subtransaction into the map
+        _txns.push_back(sub_txn);
+
+        // mark the current pgxid we are operating within
+        _cur_pg_xid = pg_xid;
+
+        return sub_txn;
+    }
+
+    void
     PgLogReader::process_log(const std::filesystem::path &path,
                              uint64_t start_offset,
                              int num_messages)
@@ -45,54 +265,64 @@ namespace springtail::pg_log_mgr {
 
                 // handle the message
                 switch(msg->msg_type) {
-                    case PgMsgEnum::BEGIN:
-                        _process_begin(std::get<PgMsgBegin>(msg->msg));
-                        break;
+                case PgMsgEnum::BEGIN:
+                    _process_begin(std::get<PgMsgBegin>(msg->msg));
+                    break;
 
-                    case PgMsgEnum::COMMIT:
-                        _process_commit(std::get<PgMsgCommit>(msg->msg));
-                        break;
+                case PgMsgEnum::COMMIT:
+                    _process_commit(std::get<PgMsgCommit>(msg->msg));
+                    break;
 
-                    case PgMsgEnum::STREAM_START:
-                        _process_stream_start(std::get<PgMsgStreamStart>(msg->msg));
-                        break;
+                case PgMsgEnum::STREAM_START:
+                    _process_stream_start(std::get<PgMsgStreamStart>(msg->msg));
+                    break;
 
-                    case PgMsgEnum::STREAM_COMMIT:
-                        _process_stream_commit(std::get<PgMsgStreamCommit>(msg->msg));
-                        break;
+                case PgMsgEnum::STREAM_COMMIT:
+                    _process_stream_commit(std::get<PgMsgStreamCommit>(msg->msg));
+                    break;
 
-                    case PgMsgEnum::STREAM_ABORT:
-                        _process_stream_abort(std::get<PgMsgStreamAbort>(msg->msg));
-                        break;
+                case PgMsgEnum::STREAM_ABORT:
+                    _process_stream_abort(std::get<PgMsgStreamAbort>(msg->msg));
+                    break;
 
-                    case PgMsgEnum::CREATE_TABLE:
-                    case PgMsgEnum::ALTER_TABLE:
+                case PgMsgEnum::INSERT:
+                case PgMsgEnum::DELETE:
+                case PgMsgEnum::UPDATE:
+                    _process_mutation(msg);
+                    break;
+
+                case PgMsgEnum::TRUNCATE:
+                    _process_truncate(std::get<PgMsgTruncate>(msg->msg));
+                    break;
+
+                case PgMsgEnum::CREATE_TABLE:
+                case PgMsgEnum::ALTER_TABLE:
                     {
                         PgMsgTable &table_msg = std::get<PgMsgTable>(msg->msg);
-                        _process_ddl(table_msg.oid, table_msg.xid, msg->is_streaming);
+                        _process_ddl(table_msg.oid, table_msg.xid, msg->is_streaming, msg);
                         break;
                     }
-                    case PgMsgEnum::DROP_TABLE:
+                case PgMsgEnum::DROP_TABLE:
                     {
                         PgMsgDropTable &drop_msg = std::get<PgMsgDropTable>(msg->msg);
-                        _process_ddl(drop_msg.oid, drop_msg.xid, msg->is_streaming);
+                        _process_ddl(drop_msg.oid, drop_msg.xid, msg->is_streaming, msg);
                         break;
                     }
-                    case PgMsgEnum::CREATE_INDEX:
+                case PgMsgEnum::CREATE_INDEX:
                     {
                         const auto &index_msg = std::get<PgMsgIndex>(msg->msg);
-                        _process_ddl(index_msg.oid, index_msg.xid, msg->is_streaming);
+                        _process_ddl(index_msg.oid, index_msg.xid, msg->is_streaming, msg);
                         break;
                     }
-                    case PgMsgEnum::DROP_INDEX:
+                case PgMsgEnum::DROP_INDEX:
                     {
                         const auto &index_msg = std::get<PgMsgDropIndex>(msg->msg);
-                        _process_ddl(index_msg.oid, index_msg.xid, msg->is_streaming);
+                        _process_ddl(index_msg.oid, index_msg.xid, msg->is_streaming, msg);
                         break;
                     }
-                    default:
-                        SPDLOG_WARN("Unknown message type\n");
-                        break;
+                default:
+                    SPDLOG_WARN("Unknown message type\n");
+                    break;
                 }
             }
 
@@ -113,6 +343,10 @@ namespace springtail::pg_log_mgr {
         xact->xact_lsn = begin_msg.xact_lsn;
         xact->xid = begin_msg.xid;
         _current_xact = xact;
+
+        // XXX prepare a batch for processing
+        _current_batch = std::make_shared<Batch>();
+        _batch_map.try_emplace({ begin_msg.xid, _current_batch });
     }
 
     void
@@ -135,6 +369,11 @@ namespace springtail::pg_log_mgr {
 
         _queue->push(xact);
         _current_xact = nullptr;
+
+        // update the write cache and system tables as needed
+        _current_batch->commit();
+        _batch_map.erase(commit_msg.xid);
+        _current_batch = nullptr;
     }
 
     void
@@ -143,6 +382,10 @@ namespace springtail::pg_log_mgr {
         SPDLOG_DEBUG_MODULE(LOG_PG_LOG_MGR, "Stream start: xid={}, first={}\n", start_msg.xid, start_msg.first);
 
         if (!start_msg.first) {
+            auto itr = _batch_map.find(start_msg.xid);
+            assert(itr != _batch_map.end());
+
+            _current_batch = itr->second;
             return;
         }
 
@@ -159,6 +402,10 @@ namespace springtail::pg_log_mgr {
         stream_xact->xid = start_msg.xid;
 
         _queue->push(stream_xact);
+
+        // XXX prepare a batch for processing
+        _current_batch = std::make_shared<Batch>();
+        _batch_map.try_emplace({ start_msg.xid, _current_batch });
     }
 
     void
@@ -185,6 +432,10 @@ namespace springtail::pg_log_mgr {
 
         _xact_map.erase(itr);
         _queue->push(xact);
+
+        // commit the current batch
+        _current_batch->commit();
+        _batch_map.erase(commit_msg.xid);
     }
 
     void
@@ -208,16 +459,41 @@ namespace springtail::pg_log_mgr {
             _queue->push(xact);
             // remove it from the map
             _xact_map.erase(itr);
+
+            // abort the txn
+            _current_batch->abort(abort_msg.xid);
+            _batch_map.erase(abort_msg.xid);
         } else {
             // subtransaction aborted, add to parent xact aborted list
             itr->second->aborted_xids.insert(abort_msg.sub_xid);
+
+            // abort the subtxn
+            _current_batch->abort_subtxn(abort_msg.sub_xid);
+            _batch_map.erase(abort_msg.sub_xid);
         }
 
+        _current_batch = nullptr;
     }
 
     void
-    PgLogReader::_process_ddl(uint32_t oid, int32_t xid, bool is_streaming)
+    PgLogReader::_process_mutation(int32_t tid,
+                                   const PgMsgTupleData &msg)
     {
+        _current_batch->add_mutation(tid, msg);
+    }
+
+    void
+    PgLogReader::_process_truncate(const PgMsgTruncate &msg)
+    {
+        _current_batch->truncate(msg);
+    }
+
+    void
+    PgLogReader::_process_ddl(uint32_t oid, int32_t xid, bool is_streaming, PgMsgPtr msg)
+    {
+        // record the schema change into the batch
+        _current_batch->schema_change(oid, xid, msg);
+
         if (!is_streaming) {
             // not streaming, so xid is invalid; use current_xact
             _current_xact->oids.insert(oid);
