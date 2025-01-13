6e675dfc8d741c7ac64e99200081c1f69f99c75e
Arun <arun@springtail.io>
2025-01-13 15:04:24 +0530

    Add new SCHEMA test cases, Renamed file to adhere to a format

---
+++ python/testing/test_cases/basic/NOTNULL_CONS_DML_Alter_Drop_Insert_Update.sql
@@ -0,0 +1,37 @@
+## test
+CREATE TABLE IF NOT EXISTS table_with_notnull_constraint_alter_drop (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Insert
+INSERT INTO table_with_notnull_constraint_alter_drop (username, email)
+VALUES ('user2', 'user2@example.com');
+
+-- Update
+UPDATE table_with_notnull_constraint_alter_drop
+SET email = 'newuser2@example.com'
+WHERE username = 'user2';
+
+-- Drop the NOT NULL constraint
+ALTER TABLE table_with_notnull_constraint_alter_drop ALTER COLUMN username DROP NOT NULL;
+ALTER TABLE table_with_notnull_constraint_alter_drop ALTER COLUMN email DROP NOT NULL;
+
+-- Test insert with null value after dropping the constraint
+INSERT INTO table_with_notnull_constraint_alter_drop (username, email)
+VALUES (NULL, 'nulluser@example.com');
+
+-- Test update with null value after dropping the constraint
+UPDATE table_with_notnull_constraint_alter_drop
+SET email = NULL
+WHERE username = 'user2';
+
+## verify
+### schema_check public table_with_notnull_constraint_alter_drop
+SELECT * FROM table_with_notnull_constraint_alter_drop;
+
+## cleanup
+DROP TABLE IF EXISTS table_with_notnull_constraint_alter_drop;
+
---
+++ python/testing/test_cases/basic/SCHEMA_Create_Tables_In_Different_Schema.sql
@@ -0,0 +1,25 @@
+## test_disabled
+CREATE SCHEMA IF NOT EXISTS test_schema_1;
+CREATE SCHEMA IF NOT EXISTS test_schema_2;
+
+CREATE TABLE IF NOT EXISTS test_schema_1.test_table_in_schema_1 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS test_schema_2.test_table_in_schema_2 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO test_schema_1.test_table_in_schema_1 (name) VALUES ('Table in schema 1');
+INSERT INTO test_schema_2.test_table_in_schema_2 (name) VALUES ('Table in schema 2');
+
+## verify
+### schema_check test_schema_1 test_table_in_schema_1
+### schema_check test_schema_2 test_table_in_schema_2
+
+## cleanup
+DROP SCHEMA IF EXISTS test_schema_1 CASCADE;
+DROP SCHEMA IF EXISTS test_schema_2 CASCADE;
+
