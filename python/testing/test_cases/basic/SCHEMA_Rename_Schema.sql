4b37ffcdca36e348d1f2e3baef1344d5f795602e
Arun <arun@springtail.io>
2025-01-14 17:50:16 +0530

    Disable failing tests

--- python/testing/test_cases/basic/SCHEMA_Rename_Schema.sql
+++ python/testing/test_cases/basic/SCHEMA_Rename_Schema.sql
@@ -1,23 +1,23 @@
-## test_disabled
-CREATE SCHEMA IF NOT EXISTS test_schema_1;
+-- DISABLED_TEST
+-- ## test
+-- CREATE SCHEMA IF NOT EXISTS test_schema_1;
 
-CREATE TABLE IF NOT EXISTS test_schema_1.test_table_in_schema_1 (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+-- CREATE TABLE IF NOT EXISTS test_schema_1.test_table_in_schema_1 (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(100)
+-- );
 
-INSERT INTO test_schema_1.test_table_in_schema_1 (name) VALUES ('Table in schema 1');
+-- INSERT INTO test_schema_1.test_table_in_schema_1 (name) VALUES ('Table in schema 1');
 
-ALTER SCHEMA test_schema_1 RENAME TO test_schema_2;
+-- ALTER SCHEMA test_schema_1 RENAME TO test_schema_2;
 
-INSERT INTO test_schema_2.test_table_in_schema_1 (name) VALUES ('Table in schema 1 after schema move');
+-- INSERT INTO test_schema_2.test_table_in_schema_1 (name) VALUES ('Table in schema 1 after schema move');
 
-## verify
-### schema_check test_schema_2 test_table_in_schema_1
+-- ## verify
+-- ### schema_check test_schema_2 test_table_in_schema_1
 
-SELECT * FROM test_schema_2.test_table_in_schema_1;
+-- SELECT * FROM test_schema_2.test_table_in_schema_1;
 
-## cleanup
-DROP SCHEMA IF EXISTS test_schema_1 CASCADE;
-DROP SCHEMA IF EXISTS test_schema_2 CASCADE;
-
+-- ## cleanup
+-- DROP SCHEMA IF EXISTS test_schema_1 CASCADE;
+-- DROP SCHEMA IF EXISTS test_schema_2 CASCADE;
