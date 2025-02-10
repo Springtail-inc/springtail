9fb581b2abfa8727ccf164e2270c9e000f4b222d
Ella Baron <ella@springtail.io>
2025-02-10 21:49:17 +0000

    added wrapper function for server side thrift API calls to catch exception

--- src/xid_mgr/xid_mgr_service.cc
+++ src/xid_mgr/xid_mgr_service.cc
@@ -6,10 +6,12 @@ namespace springtail {
     void
     ThriftXidMgrService::ping(thrift::xid_mgr::Status& _return)
     {
-        _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
-        _return.__set_message("PONG");
+        xid_mgr::XidMgrServer::call_wrapper([&_return]() {
+            _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
+            _return.__set_message("PONG");
 
-        std::cout << "Got ping\n";
+            std::cout << "Got ping\n";
+        });
     }
 
     void
@@ -18,9 +20,11 @@ namespace springtail {
                                     const thrift::xid_mgr::xid_t xid,
                                     bool has_schema_changes)
     {
-        xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
-        server->commit_xid(db_id, xid, has_schema_changes);
-        _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
+        xid_mgr::XidMgrServer::call_wrapper([&_return, db_id, xid, has_schema_changes]() {
+            xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
+            server->commit_xid(db_id, xid, has_schema_changes);
+            _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
+        });
     }
 
     void
@@ -28,17 +32,22 @@ namespace springtail {
                                            const int64_t db_id,
                                            const thrift::xid_mgr::xid_t xid)
     {
-        xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
-        server->record_ddl_change(db_id, xid);
-        _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
+        xid_mgr::XidMgrServer::call_wrapper([&_return, db_id, xid]() {
+            xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
+            server->record_ddl_change(db_id, xid);
+            _return.__set_status(thrift::xid_mgr::StatusCode::SUCCESS);
+        });
     }
 
     thrift::xid_mgr::xid_t
     ThriftXidMgrService::get_committed_xid(const int64_t db_id,
                                            const thrift::xid_mgr::xid_t schema_xid)
     {
-        xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
-        uint64_t xid = server->get_committed_xid(db_id, schema_xid);
+        uint64_t xid;
+        xid_mgr::XidMgrServer::call_wrapper([db_id, schema_xid, &xid]() {
+            xid_mgr::XidMgrServer *server = xid_mgr::XidMgrServer::get_instance();
+            xid = server->get_committed_xid(db_id, schema_xid);
+        });
         return xid;
     }
 }
