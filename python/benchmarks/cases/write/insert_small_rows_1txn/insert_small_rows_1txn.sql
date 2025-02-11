685de6075608f0545fca08475be723149aeab194
Cliff Frey <cliff@springtail.io>
2025-02-11 13:37:05 -0800

    Fix ntxn benchmarks to actually do one transaction per op (#283)
    It turns out that within a pl/pgsql block, it was all one transaction
    no matter what. This fixes that (although, for whatever reason, I
    don't think that I could get autocommit to work within one execute
    call, so I need the explicit BEGIN TRANSACTION and COMMIT statements,
    but that is fin).
    
    Example output:
    
    ```
    Benchmark: insert_large_rows_1txn
       Primary setup time: 6.473634243011475
       Setup sync time: 11.797475337982178
    
    Benchmark: insert_large_rows_ntxn
       Primary setup time: 1.8188481330871582
       Setup sync time: 8.20095944404602
    
    Benchmark: insert_small_rows_1txn
       Primary setup time: 0.6487438678741455
       Setup sync time: 7.07426643371582
    
    Benchmark: insert_small_rows_ntxn
       Primary setup time: 0.2786896228790283
       Setup sync time: 7.660956859588623
    ```
    
    I verified that many transactions are being committed by looking at
    /opt/springtail/logs/gc.log after running the benchmark.

--- python/benchmarks/cases/write/insert_small_rows_1txn/insert_small_rows_1txn.sql
+++ python/benchmarks/cases/write/insert_small_rows_1txn/insert_small_rows_1txn.sql
@@ -1,15 +1,7 @@
 BEGIN TRANSACTION;
 
-DO $$
-DECLARE
-    i INT;
-    random_text BYTEA;
-BEGIN
-    FOR i IN 1..100000 LOOP
-        INSERT INTO benchmark_data (id, value)
-        VALUES (i, gen_random_bytes(16));
-    END LOOP;
-END;
-$$;
+## repeat 100000 times
+INSERT INTO benchmark_data (value) VALUES (gen_random_bytes(16));
+## endrepeat
 
 COMMIT;
