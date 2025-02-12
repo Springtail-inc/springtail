6ac87c6c2140b4ab50201e40f3692df48f7b78ab
eg <1139932+egladysh@users.noreply.github.com>
2025-02-12 09:28:36 -0800

    checkpoint

--- python/benchmarks/cases/index/sort_empty/sort_empty_test.sql
+++ python/benchmarks/cases/index/sort_empty/sort_empty_test.sql
@@ -3,7 +3,7 @@ DECLARE
     i INT;
     v record;
 BEGIN
-    FOR i IN 1..10000 LOOP
+    FOR i IN 1..100000 LOOP
         EXECUTE format('select * from index_test_data order by col2') INTO v;
     END LOOP;
 END;
