a7b461a02a89cd5319c18d9f902d6ca13581879b
Garth Goodson <garth@springtail.io>
2025-04-01 14:16:26 -0700

    Update package.sh to include python.

--- package.sh
+++ package.sh
@@ -3,6 +3,9 @@ if [ ! -e './install' ]; then
 	echo "Run 'make install' first"
 	return 1
 fi
-mkdir -p releases
 CWD=`pwd`
-tar cpvfz releases/"springtail-$(date +%Y%m%d).tgz" -C ./install/ . -C $CWD shared-lib/
+find python -name "*.py" -print0 | tar --null -cvf release.tar --exclude='benchmarks' --exclude='testing' --files-from=-
+tar --append -f release.tar -C ./install/ . -C $CWD shared-lib/
+gzip release.tar
+mv release.tar.gz releases/"springtail-$(date +%Y%m%d).tgz"
+#tar cpvfz releases/"springtail-$(date +%Y%m%d).tgz" -C ./install/ . -C $CWD shared-lib/ -C python/shared python/shared -C python/coordinator python/coordinator
