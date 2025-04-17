0eddb35fabc996fb1da4b0fe164d1dd2c403d94e
Ella Baron <ella@springtail.io>
2025-04-17 14:22:39 +0000

    fixed alignment in xid dump

--- src/xid_mgr/test/dump_xact_log.cc
+++ src/xid_mgr/test/dump_xact_log.cc
@@ -1,4 +1,6 @@
 #include <fmt/format.h>
+#include <cstdint>
+#include <string>
 
 #include <common/init.hh>
 
@@ -22,14 +24,31 @@ int main(int argc, char *argv[])
     springtail_init_custom(service_runners);
 
     PgXactLogReader reader(log_dir);
-    reader.begin(true);
+    bool has_more = reader.begin(true);
+    uint32_t pg_xid_max_len = 0;
+    uint32_t xid_max_len = 0;
 
-    bool has_more;
-    do {
-        std::cout << fmt::format("pgxid: {}, xid: {}, real_commit: {}",
+    while (has_more) {
+        std::string pg_xid_str = std::to_string(reader.get_pg_xid());
+        if (pg_xid_str.length() > pg_xid_max_len) {
+            pg_xid_max_len = pg_xid_str.length();
+        }
+        std::string xid_str = std::to_string(reader.get_xid());
+        if (xid_str.length() > xid_max_len) {
+            xid_max_len = xid_str.length();
+        }
+        has_more = reader.next();
+    };
+
+    std::string format_str = fmt::format("pgxid: {{:>{}}} | xid: {{:>{}}} | real_commit: {{}}",
+                                         pg_xid_max_len + 1, xid_max_len + 1);
+    has_more = reader.begin(true);
+
+    while (has_more) {
+        std::cout << fmt::format(fmt::runtime(format_str.c_str()),
                                  reader.get_pg_xid(), reader.get_xid(), reader.get_real_commit()) << std::endl;
         has_more = reader.next();
-    } while (has_more);
+    };
 
     springtail_shutdown();
 }
