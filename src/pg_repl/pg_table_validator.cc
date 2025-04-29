8031c81d7a7df1b29d6beea4ccadb95070bb5361
Arun K <arun@springtail.io>
2025-04-29 07:19:59 +0530

    [SPR-710] Fix an issue with the invalid table missing the in mem cache and always looking up redis (#411)
    * [SPR-710] Fix an issue with the invalid table missing the in mem cache and always looking up redis
    
    * [SPR-710] Go back to lock_guard instead of shared_mutex and unique_lock
    
    ---------
    
    Co-authored-by: Garth Goodson <garth@springtail.io>

--- src/pg_repl/pg_table_validator.cc
+++ src/pg_repl/pg_table_validator.cc
@@ -5,22 +5,18 @@ namespace springtail
     void
     TableValidator::mark_invalid(uint64_t table_oid, const nlohmann::json& table_info)
     {
-        auto field_key = fmt::format("{}", table_oid);
-        _cache.hset(field_key, table_info.dump());
+        _cache.hset(table_oid, table_info.dump());
     }
 
     bool
     TableValidator::check_invalid(uint64_t table_oid)
     {
-        auto field_key = fmt::format("{}", table_oid);
-        auto table_info = _cache.hget(field_key);
-        return table_info.has_value();
+        return _cache.hget(table_oid).has_value();
     }
 
     void
     TableValidator::mark_valid(uint64_t table_oid)
     {
-        auto field_key = fmt::format("{}", table_oid);
-        _cache.hdel(field_key);
+        _cache.hdel(table_oid);
     }
 } // namespace springtail
