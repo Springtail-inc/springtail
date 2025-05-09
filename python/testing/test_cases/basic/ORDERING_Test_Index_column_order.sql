e56c2a7455f9565cfac2def6d93f168593dfe1f0
Deepak Babu <deepak@springtail.io>
2025-05-09 20:48:25 +0530

    [SPR-737]: Maintain index columns order based on idx_position in the DDL returned

---
+++ python/testing/test_cases/basic/ORDERING_Test_Index_column_order.sql
@@ -0,0 +1,47 @@
+## metadata
+### autocommit false
+
+## test
+BEGIN;
+CREATE TABLE index_col_ordering(
+    col1 INT NOT NULL,
+    col2 VARCHAR(100) NOT NULL,
+    col3 DATE,
+    col4 DECIMAL(10, 2),
+    col5 BOOLEAN,
+    col6 TEXT,
+    col7 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    col8 CHAR(1),
+    col9 FLOAT
+);
+COMMIT;
+
+BEGIN;
+INSERT INTO index_col_ordering (col1, col2, col3, col4, col5, col6, col8, col9) 
+VALUES 
+(1, 'value_3', '2025-02-09', 120.00, FALSE, 'value_90', 'Y', 12.0),
+(2, 'value_90', '2025-02-10', 450.25, TRUE, 'value_91', 'N', 45.2);
+COMMIT;
+
+-- Add Secondary Index with different column order than the table
+BEGIN;
+CREATE INDEX index_col_ordering_idx_col6_col2 ON index_col_ordering (col6, col2);
+COMMIT;
+
+SELECT pg_sleep(1);
+
+BEGIN;
+INSERT INTO index_col_ordering (col1, col2, col3, col4, col5, col6, col8, col9) 
+VALUES 
+(1, 'value_3', '2025-02-09', 120.00, FALSE, 'value_90', 'Y', 12.0),
+(2, 'value_90', '2025-02-10', 450.25, TRUE, 'value_91', 'N', 45.2);
+COMMIT;
+
+## verify
+### schema_check public index_col_ordering
+SELECT col1, col2, col3 FROM index_col_ordering ORDER BY col6 ASC, col2 ASC;
+
+## cleanup
+BEGIN;
+DROP TABLE IF EXISTS index_col_ordering CASCADE;
+COMMIT;
--- src/sys_tbl_mgr/service.cc
+++ src/sys_tbl_mgr/service.cc
@@ -209,7 +209,10 @@ Service::_create_index(const proto::IndexRequest& request)
         column_json["position"] = column.position();
         column_json["name"] = column.name();
 
-        ddl["columns"].push_back(column_json);
+        if (ddl["columns"].size() <= column.idx_position()) {
+            ddl["columns"].get_ref<nlohmann::json::array_t&>().resize(column.idx_position() + 1);
+        }
+        ddl["columns"][column.idx_position()] = column_json;
     }
 
     // update index names
