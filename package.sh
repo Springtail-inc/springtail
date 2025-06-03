e8bc3d6d5418467dac04a836e3477403c082f642
Ella Baron <ella@springtail.io>
2025-06-03 03:30:23 +0000

    changed release.sh to include timestamp and commit sha substring into package file name

--- package.sh
+++ package.sh
@@ -9,4 +9,5 @@ mkdir -p releases
 find python -name "*.py" -print0 | tar --null -cvf release.tar --exclude='benchmarks' --exclude='testing' --files-from=-
 tar --append -vf release.tar -C ./install/ . -C $CWD shared-lib/
 gzip release.tar
-mv release.tar.gz releases/"springtail-$(date +%Y%m%d).tgz"
+hash=$(git log -1 --format=%H -- prod.system.settings.json | cut -c1-7)
+mv release.tar.gz releases/"springtail-$(date +%Y%m%d%H%M)-${hash}.tgz"
