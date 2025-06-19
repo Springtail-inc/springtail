e1b995baac9ce2164aef5190883684d7d51a1c0d
Deepak Babu <deepak@springtail.io>
2025-06-19 15:46:01 +0530

    [SPR-842] Add some timeouts

--- python/testing/test_cases/basic/NOTNULL_CONS_Alter_Add_Constraint.sql
+++ python/testing/test_cases/basic/NOTNULL_CONS_Alter_Add_Constraint.sql
@@ -9,7 +9,7 @@ ALTER TABLE table_without_notnull_constraint ALTER COLUMN username SET NOT NULL;
 ALTER TABLE table_without_notnull_constraint ALTER COLUMN email SET NOT NULL;
 
 ## verify
-### schema_check public table_without_notnull_constraint
+### schema_check public table_without_notnull_constraint 15
 
 ## cleanup
 DROP TABLE IF EXISTS table_without_notnull_constraint;
