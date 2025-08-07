7ad9f754ac18b914101108df24899e46b15ea3c0
Arun <arun@springtail.io>
2025-08-07 13:18:43 +0000

    Update the extn parser to support operators

--- src/pg_ext/extn_parser.cc
+++ src/pg_ext/extn_parser.cc
@@ -12,11 +12,7 @@ extern "C" {
 
 std::vector<std::string> WHITELIST_EXTNS = {"pg_trgm"};
 
-struct FunctionInfo {
-    std::string name;
-    // Map of arg name to arg type
-    std::vector<std::string> args;
-};
+using DataMap = std::unordered_map<std::string, std::vector<std::pair<std::string, std::string>>>;
 
 void
 dump_parse_tree(const std::string &query)
@@ -82,7 +78,86 @@ std::string clean_sql_content(const std::string& sql) {
     return clean_sql;
 }
 
-void parse_sql(const std::string& sql, std::unordered_map<std::string, FunctionInfo>& functions) {
+void parse_function(const nlohmann::json& st, DataMap& functions){
+    if (st.contains("CreateFunctionStmt")) {
+        const auto& func = st["CreateFunctionStmt"];
+        std::string func_name;
+        std::vector<std::pair<std::string, std::string>> args;
+
+        if (func.contains("funcname")) {
+            for (const auto& n : func["funcname"]) {
+                if (n.contains("String")) {
+                    func_name += n["String"]["sval"].get<std::string>() + ".";
+                }
+            }
+            if (!func_name.empty()) func_name.pop_back();
+        }
+
+        if (func.contains("parameters")) {
+            for (const auto& param : func["parameters"]) {
+                if (param.contains("FunctionParameter")) {
+                    const auto& fp = param["FunctionParameter"];
+                    std::string type;
+                    if (fp.contains("argType")) {
+                        const auto& tn = fp["argType"];
+                        if (tn.contains("names")) {
+                            for (const auto& n : tn["names"]) {
+                                if (n.contains("String")) {
+                                    type += n["String"]["sval"].get<std::string>() + ".";
+                                }
+                            }
+                            if (!type.empty()) type.pop_back();
+                        }
+                    }
+                    args.push_back(std::make_pair(type, ""));
+                }
+            }
+        }
+
+        functions[func_name] = args;
+    }
+}
+
+void parse_operator(const nlohmann::json& st, DataMap& operators){
+    if (st.contains("DefineStmt")) {
+        const auto& def = st["DefineStmt"];
+        const auto& kind = def["kind"];
+        if (kind == "OBJECT_OPERATOR") {
+            std::vector<std::pair<std::string, std::string>> args;
+            std::string op_name;
+            if (def.contains("defnames")){
+                const auto& opname = def["defnames"][0]["String"]["sval"].get<std::string>();
+                op_name = opname;
+            }
+            if (def.contains("definition")) {
+                const auto& defn = def["definition"];
+                for (const auto& d : defn) {
+                    if (d.contains("DefElem")) {
+                        const auto& de = d["DefElem"];
+                        const auto& defname = de["defname"];
+                        if (de.contains("arg")) {
+                            const auto& arg = de["arg"];
+                            if (arg.contains("TypeName")) {
+                                const auto& tn = arg["TypeName"];
+                                if (tn.contains("names")) {
+                                    for (const auto& n : tn["names"]) {
+                                        if (n.contains("String")) {
+                                            args.push_back(std::make_pair(defname, n["String"]["sval"]));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            operators[op_name] = args;
+        }
+    }
+}
+
+// XXX: Handle alter and versioning
+void parse_sql(const std::string& sql, DataMap& functions, DataMap& operators) {
     std::string clean_sql = clean_sql_content(sql);
 
     if (clean_sql.empty()) {
@@ -100,43 +175,8 @@ void parse_sql(const std::string& sql, std::unordered_map<std::string, FunctionI
 
     for (const auto& stmt : parse_tree["stmts"]) {
         auto st = stmt["stmt"];
-        if (st.contains("CreateFunctionStmt")) {
-            const auto& func = st["CreateFunctionStmt"];
-            std::string func_name;
-            std::vector<std::string> args;
-
-            if (func.contains("funcname")) {
-                for (const auto& n : func["funcname"]) {
-                    if (n.contains("String")) {
-                        func_name += n["String"]["sval"].get<std::string>() + ".";
-                    }
-                }
-                if (!func_name.empty()) func_name.pop_back();
-            }
-
-            if (func.contains("parameters")) {
-                for (const auto& param : func["parameters"]) {
-                    if (param.contains("FunctionParameter")) {
-                        const auto& fp = param["FunctionParameter"];
-                        std::string type;
-                        if (fp.contains("argType")) {
-                            const auto& tn = fp["argType"];
-                            if (tn.contains("names")) {
-                                for (const auto& n : tn["names"]) {
-                                    if (n.contains("String")) {
-                                        type += n["String"]["sval"].get<std::string>() + ".";
-                                    }
-                                }
-                                if (!type.empty()) type.pop_back();
-                            }
-                        }
-                        args.push_back(type);
-                    }
-                }
-            }
-
-            functions[func_name] = {func_name, args};
-        }
+        parse_function(st, functions);
+        parse_operator(st, operators);
     }
 
     pg_query_free_parse_result(result);
@@ -148,17 +188,24 @@ std::string read_file(const std::string& path) {
     return std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
 }
 
-void print_functions(const std::unordered_map<std::string, FunctionInfo>& functions){
+void print_functions(const DataMap& functions){
     std::cout << "Functions:\n";
     for (const auto& func : functions) {
-        std::cout << func.first << "(";
-        for (size_t i = 0; i < func.second.args.size(); ++i) {
-            std::cout << func.second.args[i];
-            if (i < func.second.args.size() - 1) {
-                std::cout << ", ";
-            }
+        std::cout << "FUNC: " << func.first << "(";
+        for (const auto& [arg, type] : func.second) {
+            std::cout << arg << ", ";
         }
         std::cout << ")\n";
+    }
+}
+
+void print_operators(const DataMap& operators){
+    std::cout << "Operators:\n";
+    for (const auto& func : operators) {
+        std::cout << "OP: " << func.first << std::endl;
+        for (const auto& arg : func.second) {
+            std::cout << "ARG: " << arg.first << ", TYPE: " << arg.second << std::endl;
+        }
     }
 }
 
@@ -178,19 +225,20 @@ void process_extension_dir(const std::filesystem::path& ext_dir) {
         }
     }
 
-    std::unordered_map<std::string, FunctionInfo> functions;
+    DataMap functions;
+    DataMap operators;
 
     for (const auto& [extn, files] : versioned_files) {
         std::cout << "Extension: " << extn << std::endl;
         for (const auto& file : files) {
             std::cout << "  " << file << std::endl;
             std::string sql = read_file(file);
-            parse_sql(sql, functions);
+            parse_sql(sql, functions, operators);
         }
     }
 
     print_functions(functions);
-
+    print_operators(operators);
 }
 
 int
@@ -199,19 +247,6 @@ main()
     std::string base_dir = "/usr/share/postgresql/16/extension";
 
     process_extension_dir(base_dir);
-
-    // std::string query = "\\echo Use \"ALTER EXTENSION pg_trgm UPDATE TO '1.5'\" to load this fileCREATE FUNCTION set_limit(float4) RETURNS float4 AS 'MODULE_PATHNAME' LANGUAGE C STRICT VOLATILE PARALLEL UNSAFE;";
-
-    // Iterate the SQL files from base_dir, based on the whitelist extensions
-
-    // std::unordered_map<std::string, FunctionInfo> functions;
-    // parse_sql(query, functions);
-
-    // query = "CREATE FUNCTION show_trgm(text) RETURNS _text AS 'MODULE_PATHNAME' LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;";
-    // parse_sql(query, functions);
-
-    // query = "CREATE FUNCTION similarity(text,text) RETURNS float4 AS 'MODULE_PATHNAME' LANGUAGE C STRICT IMMUTABLE PARALLEL SAFE;";
-    // parse_sql(query, functions);
 
     return 0;
 }
