8251dd306cf2e9e8c8ba7d128cab778a11bac415
Ella Baron <ella@springtail.io>
2025-08-19 18:06:27 +0000

    another bug

--- include/pg_fdw/pg_fdw_ddl_common.hh
+++ include/pg_fdw/pg_fdw_ddl_common.hh
@@ -347,8 +347,12 @@ namespace springtail::pg_fdw {
             auto fields = table->extent_schema()->get_fields();
             for (auto row : (*table)) {
                 uint64_t tid = fields->at(sys_tbl::Schemas::Data::TABLE_ID)->get_uint64(&row);
+                uint64_t xid = fields->at(sys_tbl::Schemas::Data::XID)->get_uint64(&row);
+                if (xid > schema_xid) {
+                    continue;
+                }
 
-                LOG_DEBUG(LOG_FDW, "Found table in schemas table: {}", tid);
+                LOG_DEBUG(LOG_FDW, "Found table in schemas table: {}, xid: {}, schema_xid: {}", tid, xid, schema_xid);
 
                 // check if we have moved to next tid
                 if (tid != current_tid) {
