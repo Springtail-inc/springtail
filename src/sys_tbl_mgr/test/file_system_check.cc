fe606fcf0a70f608087a2ffb33e6a5642f1eb190
ella-springtail <ella.springtail@gmail.com>
2025-08-25 09:55:20 -0700

    Spr 865 fs snapshot validation (#573)
    * Added snapshot validation
    
    * code quality improvement
    
    * fixed snapshot xid
    
    * added explicit
    
    * fixed some sonar errors
    
    * another sonar fix
    
    * added the ability to verify file system for every xid up to maximum given xid
    
    * made function constant
    
    * removed function call
    
    * bug fix
    
    ---------
    
    Co-authored-by: Ella Baron <ella@springtail.io>

--- src/sys_tbl_mgr/test/file_system_check.cc
+++ src/sys_tbl_mgr/test/file_system_check.cc
@@ -1,15 +1,30 @@
+#include <boost/program_options.hpp>
+
 #include <common/init.hh>
 
 #include <test/file_system_check.hh>
+
 using namespace springtail;
+namespace po = boost::program_options;
 
 int
 main(int argc, char *argv[])
 {
+    uint64_t max_xid = 0;
+    bool all_xids = false;
+
+    po::options_description desc("Options");
+    desc.add_options()("max_xid,mx", po::value<uint64_t>(&max_xid)->default_value(constant::LATEST_XID), "Maximum xid, default is the latest");
+    desc.add_options()("all_xids,ax", po::value<bool>(&all_xids)->default_value(false), "Flag to check all xids, default is the false");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
     // no logging
     springtail_init(false, std::nullopt, LOG_NONE);
 
-    std::shared_ptr<test::FSCheck> fs_check = std::make_shared<test::FSCheck>();
+    auto fs_check = std::make_shared<test::FSCheck>(max_xid, all_xids);
     fs_check->check_dbs();
     fs_check.reset();
 
