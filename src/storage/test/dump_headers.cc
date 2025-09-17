f48aebf8fa0e98ec046649cfcf6b6f0f96c19ea0
Garth Goodson <garth@springtail.io>
2025-09-17 09:14:45 -0700

    Uncommented some code commented for debugging.

--- src/storage/test/dump_headers.cc
+++ src/storage/test/dump_headers.cc
@@ -21,13 +21,13 @@ main(int argc,
     uint64_t max = std::filesystem::file_size(argv[1]);
     while (pos < max) {
         auto response = handle->read(pos);
-/*
+
         Extent e(response->data);
         std::cout << fmt::format("Extent @ {}", pos) << std::endl
                   << fmt::format("\ttype: {}, xid: {}, row_size: {}, prev: {}",
                                  static_cast<uint8_t>(e.header().type), e.header().xid,
                                  e.header().row_size, e.header().prev_offset) << std::endl;
-*/
+
         pos += response->next_offset;
     }
     springtail_shutdown();
