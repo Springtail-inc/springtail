08d55d3f4d27f5ddfa45d598e0da165a666eef2d
Deepak Babu <deepak@springtail.io>
2025-10-30 11:34:21 +0530

    Revert "[SPR-1010]: Increase debug query time"
    This reverts commit 909e22c86dbf62dce5e22a6e7dd22e02838bd7cd.

--- python/testing/test_cases/query_benchmark/BASIC_Select.sql
+++ python/testing/test_cases/query_benchmark/BASIC_Select.sql
@@ -5,5 +5,5 @@
 
 ## verify
 ### benchmark 100 5000 "SELECT * FROM customers"
-### benchmark 50 4000 "SELECT CustomerID FROM customers"
+### benchmark 50 2000 "SELECT CustomerID FROM customers"
 ### benchmark 70 2000 "SELECT LastName FROM customers"
